5267d4813dabba6b1ce284e9351cef5be40c8a63
amsuhane <ayushsuhane99@iitkgp.ac.in>
2020-07-27 14:15:10 +0530

    minor change

--- BERT_HAN_exp/grid_search.py
+++ BERT_HAN_exp/grid_search.py
@@ -5,15 +5,17 @@ import os
 
 hidden_sizes = [200]
 learning_rates = [1e-4]
+epochs = [5]
 
 sarcasm_files = ['../Datasets/Sarcasm/sarcasm_dataset.pkl', '../Datasets/Sarcasm/sarcasm_dataset_9.pkl', '../Datasets/Sarcasm/sarcasm_dataset_22.pkl']
 sentiment_files = ['../Datasets/Sentiment/data_frame.pkl', '../Datasets/Sentiment/data_frame_9.pkl', '../Datasets/Sentiment/data_frame_22.pkl']
 hate_files = ['../Datasets/Hate/hate_dataset.pkl', '../Datasets/Hate/hate_dataset_9.pkl', '../Datasets/Hate/hate_dataset_22.pkl']
 humour_files = ['../Datasets/Humour/humour_dataset.pkl', '../Datasets/Humour/humour_dataset_9.pkl', '../Datasets/Humour/humour_dataset_22.pkl']
 
-files = sarcasm_files[-1:]
+files = ['../Datasets/Sarcasm/sarcasm_dataset_22.pkl', '../Datasets/Hate/hate_dataset_22.pkl', '../Datasets/Humour/humour_dataset_22.pkl']
+files = files[:1]
 
 for fil in files:
     for lr in learning_rates:
         for hs in hidden_sizes:
-            os.system(f'python bert_cross_val.py --lr {lr} --hs {hs} --filename {fil}')
+            os.system(f'python bert_cross_val.py --lr {lr} --hs {hs} --filename {fil} -e {epochs}')
